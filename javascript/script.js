be4d4b930a6bef2ec2fa1b169e1474bfbe632f13
davantiauncaffe <60547571+davantiauncaffe@users.noreply.github.com>
2020-02-01 16:32:39 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "leggi tutto";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "riduci";
+    moreText.style.display = "inline";
+  }
+}
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
