897ce84ccddf37528bba55e2cdd56da09cd1ff59
davantiauncaffe <60547571+davantiauncaffe@users.noreply.github.com>
2020-06-19 16:18:57 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,49 +1,151 @@
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+ <!doctype html>
+<html lang="en">
+ <head> 
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "leggi tutto";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "riduci";
-    moreText.style.display = "inline";
-  }
-}
+<META NAME="DC.TITLE" CONTENT="Davantiauncaffè">
+<META NAME="DESCRIPTION" CONTENT="Ciò che troverai qui non sarà altro che un guazzabuglio di parole, pensieri alla rinfusa, riflessioni, opinioni non richieste – il mio personale sguardo sul mondo che spero tu abbia voglia di conoscere. Prometto di non rubare troppo del tuo tempo: ti va di fermarti un po' a parlare, magari davanti a un caffè?">
+<META NAME="KEYWORDS" CONTENT=" libri, recensioni, recensione, blog, scrittura ">
+<META NAME="revisit-after" CONTENT="1 days"><META NAME="robots" CONTENT="FOLLOW,INDEX">
 
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
- 
-}
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157528873-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
+  gtag('config', 'UA-157528873-1');
+</script>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 
 
+  <script src="https://kit.fontawesome.com/d907b3af07.js" crossorigin="anonymous"></script>
+   <meta charset='utf-8'>
+   <title>Davantiauncaffè</title>
+
+  <!--LINKS-->
+
+   <link rel="icon" href="imgs/icon.ico"/>
+   <link rel="stylesheet" type="text/css" href="css/style.css"/>
+   <link rel="stylesheet" type="text/css" href="css/media.css"/>     
+   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+   <script type="text/javascript"></script>
+   <script src="javascript/script.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i&display=swap" rel="stylesheet">
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ </head>
+ <body>
+
+<!-- Navigation  -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Chi sono?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="scrivimiqui.html">Scrivimi qui</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
 
-//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
-}
+<div class="container">
+  
+  <img class="logo" src="imgs/logounito.png">
+  <img class="separe" src="imgs/separe.png">
+  
+  <div class="row">
+  <div class="column">
+    <div class="image" id="prova"></div>
+  </div>
+  <div class="column">
+    <h2>Benvenuto/a!</h2>
+    <p id="presentazione">
+Ciò che troverai qui non sarà altro che un guazzabuglio di parole, pensieri alla rinfusa, riflessioni, opinioni non richieste – il mio personale sguardo sul mondo che spero tu abbia voglia di conoscere. Prometto di non rubare troppo del tuo tempo: ti va di fermarti un po' a parlare, magari <i>davanti a un caffè?</i>
+ </p>
+  </div>
+</div>
 
+<img class="separe" src="imgs/separe.png">
+
+<div class="row">
+  <div class="column">
+     
+<a href="https://www.instagram.com/davantiauncaffe/"><div class="image" id="seguimi"></div></a>
+
+  </div>
+  <div class="column">
+   <div class="image" id="murakami"></div>
+  </div>
+</div>
+
+
+<img class="separe" src="imgs/separe.png">
+
+
+<div class="image" id="qualcosa"></div>
+<div class="primo">
+  <h3>Agfj fjgfd dhsd hfdhd </h3>
+  <h4>16 giugno 2020</h4>
+  <p>bfhdf jfgj f jd fjdfj d fhdvjff fj fdjfdhshfd.</p>
+</div>
+
+<p style="text-align: center"><a href="qualcosa.html" style="text-decoration: underline">Clicca qui per leggere gli altri articoli</a></p>
+
+<img class="separe" src="imgs/separe.png">
+
+<div class="image" id="cineception"></div>
+<div class="primo">
+  <h3>Agfj fjgfd dhsd hfdhd </h3>
+  <h4>16 giugno 2020</h4>
+  <p>bfhdf jfgj f jd fjdfj d fhdvjff fj fdjfdhshfd.</p>
+</div>
+
+<p style="text-align: center"><a href="cineception.html" style="text-decoration: underline">Clicca qui per leggere gli altri articoli</a></p>
+
+<img class="separe" src="imgs/separe.png">
+
+<div class="image" id="racconti2"></div>
+<div class="primo">
+  <h3><a href="motoreciakazione.html">• Motore, Ciak, Azione</a></h3>
+  <h4>2 febbraio 2020 | tempo di lettura: 4 minuti</h4>
+  <p>«Salve, Mr. Pattinson. È un piacere rivederla.»<br>
+Mr. Pattinson si sbottonò la giacca, lasciandosi cadere mollemente sulla poltroncina. «Il piacere è tutto mio. Allora, come procedono i lavori?»<br>
+«Credevo si fidasse di me».<br>
+Mr. Pattinson fece spallucce. «Certamente. Ciò non toglie che io abbia diritto a qualche aggiornamento. Il contratto prevede che...»<br>
+«Non si scaldi, so bene cosa prevede il contratto. Lavoro sul campo da dodici anni e non è la prima volta che mi occupo di... un caso come il suo». <a href="motoreciakazione.html" class="continuaaleggere"><i>continua a leggere</i></a></p>
+</div>
+<p style="text-align: center"><a href="racconti.html" style="text-decoration: underline">Clicca qui per leggere gli altri racconti</a></p>
+  <img class="separe" src="imgs/separe.png">
+</div>
+<footer>_<br><br>
+
+      background pattern from <a href="https://www.toptal.com/designers/subtlepatterns/"> toptal subtle patterns </a>
+      </footer></div>
+
+ </body>
+</html>
